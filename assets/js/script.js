bbc3787748b6c5b649e4b31c9c63ab5cecb6a2c6
OnlyMeHere <bennett45.j.m@gmail.com>
2022-06-30 08:31:42 -0700

    linked CSS updated JS

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+
+// ---- current day displayed at the top of the calander
+
+
+
+
+// ---- scrolling down finds time blocks for standard business hours
+
+
+
+// --- each time block is color-coded for past, present and future
+
+
+
+// ---enter an event by clicking on the time block
+
+
+
+// --- the event is saved to local storage by clicking the SAVE button for that time block
+
+
+// --- refreshing the page finds the saved time block remains 
